82616da3f25b2a9184561a46e96d63405f5328e0
jscdavis <jdavis5005@gmail.com>
2016-08-01 20:07:42 -0400

    lab 9 commit first drafts

---
+++ messages/README.txt
@@ -0,0 +1,4 @@
+James Davis - Lab 9
+All aspects of the lab are working properly. I asked my friend Matt, a friend of mine who is a student at the University of Rochester, helped me with Javascript syntax, but other than that I didn't collaborate with anyone. I spent about 4.5 hours on this lab.
+
+When I tried to request data from both the local computer and from messagehub.herokuapp.com, but neither worked. This is because 
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here...
+request = new XMLHttpRequest();
+
+request.open("GET", "data.json", true);
+
+request.onreadystatechange = callme;
+
+request.send();
+
+		function callme () {
+			if (request.readyState == 4 && request.status == 200) {
+				raw = request.responseText;
+				parsedData = JSON.parse(raw);
+				document.getElementById("messages").innerHTML = parsedData[0].content + " " + "<h2>" + parsedData[0].username + "<br>" + "</h2>"
+				+ parsedData[1].content + " " + "<h2>" + parsedData[1].username + "</h2>";
+			}
+		};
